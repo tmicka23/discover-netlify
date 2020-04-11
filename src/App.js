cb76952dae41df4f8531982b06ac179577ee7d0b
tmicka23 <mickathp42@gmail.com>
2020-04-11 14:30:04 +0200

    change title

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="title">
-          <h1> Welcome to Netlify !</h1>
+          <h1> Bienvenue Netlify !</h1>
         </div>
       </div>
     );
